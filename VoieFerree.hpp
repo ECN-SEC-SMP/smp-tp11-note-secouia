5fe17d4f4dc71f38291f599cb5392d45408e2095
Zaryus <contact.zaryus@gmail.com>
2026-03-18 16:33:48 +0100

    Ajout du fichier VoieFerree.hpp

--- VoieFerree.hpp
+++ VoieFerree.hpp
@@ -2,18 +2,19 @@
 #include <vector>
 #include "Ville.hpp"
 #include "Joueur.hpp"
+#include "couleurEnum.hpp"
 
 using namespace std;
 
 
 class VoieFerree {
     private: 
-        string couleur; 
+        couleurTrain couleur;
         int poids; 
         vector<Ville*> listeVille;
         Joueur* joueur;
     public:
         VoieFerree(vector<Ville*> listeVille, string couleur, int poids);
         vector<Ville*> getListeVille() const;
-        
+
 };
