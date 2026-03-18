dbe56789134a0f3f3f7cb09e94fe93dbfb391fbd
Zaryus <contact.zaryus@gmail.com>
2026-03-18 16:13:26 +0100

    Ajout du hpp de Plateau

--- Plateau.hpp
+++ Plateau.hpp
@@ -1,1 +1,11 @@
 #include "VoieFerree.hpp"
+
+class Plateau {
+    private:
+        vector<VoieFerree*> listeVoieFerree; 
+    
+    public:
+        void placeWagon(string villeA, string villeB, Joueur joueur);
+        bool existeChemin(string villeA, string villeB);
+        bool existeTraversee();
+};
